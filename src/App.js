17af53fd1355c21f8cd15de92975f7d4bdbe44b1
johan <johan.fors.90@gmail.com>
2019-03-07 08:04:06 +0100

    new props work

--- src/App.js
+++ src/App.js
@@ -5,10 +5,70 @@ import Tachyons from "tachyons";
 import './App.css';
 
 class App extends Component {
+
+	constructor() {
+		super();
+		this.state = {
+			width: 0,
+			length: 0,
+			basicMaterial: 0.6,
+			topMaterial: 0.8,
+			age: 0
+		}
+	}
+
+
+	getLength = (lengthValue) => {
+		this.setState({length: parseInt(lengthValue.target.value)});
+	}
+
+	getWidth = (widthValue) => {
+		this.setState({width: parseInt(widthValue.target.value)});	
+	}
+
+	getAge = (ageValue) => {
+		if (parseInt(ageValue.target.value) >= 35) {
+			this.setState({age: 0.5})
+		} else if (parseInt(ageValue.target.value) >= 20) {
+			this.setState({age: 0.7})
+		} else if (parseInt(ageValue.target.value) >= 10) {
+			this.setState({age: 0.8})
+		} else if (parseInt(ageValue.target.value) >= 5) {
+			this.setState({age: 0.9})
+		} else {
+			this.setState({age: 1})
+		}
+	}
+
+	getBasicMaterial = (value) => {
+		this.setState({basicMaterial: value});
+	}
+
+	getTopMaterial = (value) => {
+		this.setState({topMaterial: value});
+	}
+
+	mainFunction = () => {
+		const TotalValue = (this.state.basicMaterial + this.state.topMaterial) * this.state.length * this.state.width * this.state.age;
+
+		return (
+			console.log(TotalValue)
+		);
+	}
+
+
+
   render() {
     return (
       <div className="">
-        <MainForm />
+        <MainForm 
+        	getLength={this.getLength} 
+        	getWidth={this.getWidth} 
+        	getAge={this.getAge} 
+        	getBasicMaterial={this.getBasicMaterial} 
+        	getTopMaterial={this.getTopMaterial} 
+        	mainFunction={this.mainFunction} 
+        />
       </div>
     );
   }
--- src/components/MainForm/MainForm.js
+++ src/components/MainForm/MainForm.js
@@ -1,43 +1,50 @@
 import React from "react";
 import "./MyForm.css";
 
-const MainForm = () => {
+
+const MainForm = ({getWidth, getLength, getAge, getBasicMaterial, getTopMaterial, mainFunction}) => {
     return (
         <div className="mybackground">
             <div className="pt7 pb7">
                 <article className="tc br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw9 center formbackground">
                     <div className="">
                         <p className="f3">Anslutningsberäkning</p>
-                        <div class="flex items-center justify-center flex-column">
-                          <div class='cf dib'>
-                            <p class="m0 f6 silver tc">Vägens grundmaterial</p>
-                            <a class="selectionbackground f6 fl hover-bg-orange hover-white b--black link bb bt bl ph3 pv2 dib br2 br--left bl orange" href="#0">Grus (dålig)</a>
-                            <a class="selectionbackground f6 fl hover-bg-orange hover-white b--black link ba ph3 pv2 dib orange" href="#0">Grus (bra)</a>
-                            <a class="selectionbackground f6 fl hover-bg-orange hover-white b--black link bb bt ph3 pv2 dib orange br2 br--right br" href="#0">Grus (bättre)</a>
+                        <div className="flex items-center justify-center flex-column">
+                          <div className='cf dib'>
+                            <p className="m0 f6 silver tc">Vägens grundmaterial</p>
+                            <p onClick={() => getBasicMaterial(100)} className="selectionbackground f6 fl hover-bg-orange hover-white b--black bb bt bl ph3 pv2 dib br2 br--left bl orange" href="#0">Grus (dålig)</p>
+                            <p onClick={() => getBasicMaterial(150)} className="selectionbackground f6 fl hover-bg-orange hover-white b--black ba ph3 pv2 dib orange" href="#0">Grus (bra)</p>
+                            <p onClick={() => getBasicMaterial(250)} className="selectionbackground f6 fl hover-bg-orange hover-white b--black bb bt ph3 pv2 dib orange br2 br--right br" href="#0">Grus (bättre)</p>
                           </div>
                         </div>
-                        <div class="flex items-center justify-center flex-column">
-                          <div class='cf dib'>
-                            <p class="m0 f6 silver tc">Vägens beläggning</p>
-                            <a class="selectionbackground f6 fl hover-bg-orange hover-white b--black link bb bt bl ph3 pv2 dib br2 br--left bl orange" href="#0">Ingen</a>
-                            <a class="selectionbackground f6 fl hover-bg-orange hover-white b--black link ba ph3 pv2 dib orange" href="#0">Oljegrus</a>
-                            <a class="selectionbackground f6 fl hover-bg-orange hover-white b--black link bb bt ph3 pv2 dib orange br2 br--right br" href="#0">Asfalt</a>
+                        <div className="flex items-center justify-center flex-column">
+                          <div className='cf dib'>
+                            <p className="m0 f6 silver tc">Vägens beläggning</p>
+                            <p onClick={() => getTopMaterial(0)} className="selectionbackground f6 fl hover-bg-orange hover-white b--black bb bt bl ph3 pv2 dib br2 br--left bl orange" href="#0">Ingen</p>
+                            <p onClick={() => getTopMaterial(25)}className="selectionbackground f6 fl hover-bg-orange hover-white b--black ba ph3 pv2 dib orange" href="#0">Oljegrus</p>
+                            <p onClick={() => getTopMaterial(50)}className="selectionbackground f6 fl hover-bg-orange hover-white b--black bb bt ph3 pv2 dib orange br2 br--right br" href="#0">Asfalt</p>
                           </div>
                         </div>
-                        <form class="pt3 pl5 pr5 black-80">
-                          <div class="measure ">
-                            <label for="length" class="f6 b db mb2">Vägens längd</label>
-                            <input id="length" class="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" aria-describedby="name-desc" />
+                        <form className="pt3 pl5 pr5 black-80">
+                          <div className="measure ">
+                            <label htmlFor="length" className="f6 b db mb2">Vägens ålder</label>
+                            <input onChange={getAge} id="length" className="shadow-5 input-reset ba b--black-20 pa2 mb2 db w-100" type="text" aria-describedby="name-desc" />
                           </div>
                         </form>
-                        <form class="pt3 pl5 pr5 pb3 black-80">
-                          <div class="measure">
-                            <label for="width" class="f6 b db mb2">Vägens bredd</label>
-                            <input id="width" class="input-reset ba b--black-20 pa2 mb2 db w-100" type="text" aria-describedby="name-desc" />
+                        <form className="pt3 pl5 pr5 black-80">
+                          <div className="measure ">
+                            <label htmlFor="length" className="f6 b db mb2">Vägens längd</label>
+                            <input onChange={getLength} id="length" className="shadow-5 input-reset ba b--black-20 pa2 mb2 db w-100" type="text" aria-describedby="name-desc" />
+                          </div>
+                        </form>
+                        <form className="pt3 pl5 pr5 pb3 black-80">
+                          <div className="measure">
+                            <label htmlFor="width" className="f6 b db mb2">Vägens bredd</label>
+                            <input onChange={getWidth} id="width" className="shadow-5 input-reset ba b--black-20 pa2 mb2 db w-100" type="text" aria-describedby="name-desc" />
                           </div>
                         </form>
                         <div className="center pb4">
-                          <button class="f5 b bg-orange hover-bg-orange hover-white b--black link bb bt br bl ph3 br4 pv2 dib black pointer">Beräkna
+                          <button onClick={mainFunction} className="f5 b grow bg-orange hover-bg-orange hover-white b--black bb bt br bl ph3 br4 pv2 dib black pointer">Beräkna
                           </button>
                         </div>
                     </div>
